1a09ebe7285acb0be42406b8def786ba46e4ddaf
dhimantaa <dhimantarun19@gmail.com>
2021-10-07 20:00:27 +0530

    Added changes 1 - Changes in sync to download only the file which are not available in local

--- sync/__init__.py
+++ sync/__init__.py
@@ -1,3 +1,4 @@
+import os
 import data
 import configuration
 import pandas as pd
@@ -27,6 +28,32 @@ DATA_MAPPING = [
 ]
 
 
+def create_dir(path):
+    """
+    To create directory to ump the data
+    :param path:
+    :return:
+    """
+    try:
+        if not os.path.exists(path):
+            os.makedirs(path)
+        return True
+    except Exception as e:
+        return False
+
+
+def check_file(file_path):
+    """
+    Check if file exsist
+    :param file_path:
+    :return:
+    """
+    if os.path.isfile(file_path):
+        return True
+    else:
+        return False
+
+
 class SYNC:
     def __init__(self, date_range):
         """
@@ -37,19 +64,26 @@ class SYNC:
 
     def sync(self):
         """
-
+        Sync the data to the local
         :return:
         """
 
         for date in self.date_range:
-
             str_date = date.strftime("%Y-%m-%d")
-            df = data.match_data(str_date)
-            df.to_csv("data/dump/{}.csv".format(str_date))
-            for match_index in range(0, len(df['match_report'].tolist())):
-                dt = pd.read_html(df['match_report'].tolist()[match_index])
-                for d in range(0, len(dt)):
-                    dt[d].to_csv(
-                        f"{self.config['SYNC']['PATH']}{DATA_MAPPING[d]}_match_{match_index}_{str_date}.csv"
-                    )
-            print("Data dump successful for the date {}".format(str_date))
+            path = "{0}\{1}".format(self.config['SYNC']['PATH'], str_date)
+            if create_dir(path):
+                df = data.match_data(str_date)
+                if not check_file("{0}\{1}.csv".format(path,str_date)):
+                    df.to_csv("{0}\{1}.csv".format(path, str_date))
+                for match_index in range(0, len(df['match_report'].tolist())):
+                    filename = "{0}_{1}".format(df['team_a'].tolist()[match_index], df['team_b'].tolist()[match_index])
+                    dt = pd.read_html(df['match_report'].tolist()[match_index])
+                    for d in range(0, len(dt)):
+                        if not check_file(f"{path}\{filename}_{DATA_MAPPING[d]}_match_report.csv"):
+                            dt[d].to_csv(
+                                f"{path}\{filename}_{DATA_MAPPING[d]}_match_report.csv"
+                            )
+                print("Data dump successful for the date {}".format(str_date))
+            else:
+                pass
+
